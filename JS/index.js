e1bc83405d6e8189b2d56bb454342c56dc97d3e3
Alireza <arbor0711@gmail.com>
2023-04-06 23:29:23 +0200

    Add scroll to top animation

--- JS/index.js
+++ JS/index.js
@@ -83,9 +83,9 @@ setInterval(onNextClick, 6000);
 // Scroll To Top Button
 
 let scrollToTopBtn = document.querySelector("#scrollToTopBtn");
-scrollToTopBtn.addEventListener("click", topFunction);
+scrollToTopBtn.addEventListener("click", scrollToTop);
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function scrollFunction() {
+window.onscroll = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopBtn.style.display = "block";
   } else {
@@ -93,7 +93,10 @@ window.onscroll = function scrollFunction() {
   }
 };
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function scrollToTop() {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
 }
